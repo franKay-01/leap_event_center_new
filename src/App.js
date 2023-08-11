325eb9b4fd604ad12e901bace4a13ae53f761cd5
franKay-01 <fkay0450@gmail.com>
2023-08-11 11:54:32 +0000

    feat: working on home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import dashboardImg from './assets/dashboard_new.gif'
-import winter_tree from './assets/winter_tree.png';
-import BackdropImg from './assets/leap_eventbackdrop.jpg';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import Navbar from './components/navbar';
 import HomeBannerVid from './assets/home_banner.mp4'
-import ArrowGif from './assets/arrow_down.gif'
 
 function App() {
-  useEffect(()=>{
-  
-  },[])
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const submitEmail = () => {
+    if (email === ""){
+      alert("Email can not be empty")
+      return
+    }
+
+    setIsLoading(true)
+    const formData = new URLSearchParams();
+    formData.append('Emails', email);
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbyrY6Ekx0FTlFk0OXtLhn52l1VCGtXuOlssF0V50dGCjvEqZCXPaoviaRB-jFCn58q6ng/exec",
+      {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+        }
+      }
+    ).then((res) => {
+      console.log(JSON.stringify(res))
+      res.json()
+    }).then((data) => {
+      const message = "Your message was successfully submitted!"
+      setEmail('')
+      setIsLoading(false)
+      alert(message)
+      console.log("RESP "+data);
+    })
+    .catch((error) => {
+      console.log("ERR "+error);
+    });
+  }
 
   return (
     <>
@@ -37,10 +67,9 @@ function App() {
             </div>
           </div>
           <div className='absolute bottom-12'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 white-label cursor-pointer">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
-          </svg>
-
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 white-label cursor-pointer">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+            </svg>
           </div>
         </div>
         <div class="gradLine3"></div>
@@ -101,33 +130,9 @@ function App() {
               </h1>
             </div>
           </div>
-          {/* <div className='grid grid-cols-2 mt-52'>
-            <div className='flex justify-center items-center p-8 mission-bg'>
-              <h1 className='mission-header'>Our Mission</h1>
-            </div>
-            <div className='flex'>
-              <img className='mission-img' src={MissionImg}/>
-            </div>
-            <div className='flex'>
-              <img className='mission-img' src={VisionImg}/>
-            </div>
-            <div className='flex justify-center items-center p-8 mission-bg'>
-              <h1 className='mission-subs'>
-                At Leap Events Center, our mission is to provide our clients with exceptional event spaces, unparalleled customer service, and turnkey solutions to create successful and memorable events.
-              </h1>
-            </div>
-            <div className='flex justify-center items-center p-8 mission-bg'>
-              <h1 className='mission-subs'>
-              We strive to offer up-to-date amenities in an elegant and modern setting to inspire creativity and cultivate inspiration to bring our clients' visions to life.
-              </h1>
-            </div>
-            <div className='flex'>
-              <img className='mission-img' src={GoalImg}/>
-            </div>
-          </div> */}
         </div>
         <div className='container'>
-          <a href='#' class="ribbon-wrapper mt-8">
+          <a href='/#' class="ribbon-wrapper mt-8">
             <div class="ribbon-content">
               <span class="ribbon">20% off</span>
               </div>
@@ -140,6 +145,40 @@ function App() {
                 </div>
               </p>
           </a>
+        </div>
+        <div class="contact-h bg-gradient-to-r from-white to-yellow-700 mt-24">
+          <div className='container grid grid-cols-1'>
+            <div className='flex flex-col'>
+              <div className='contact-us-header'>Contact Us!</div>
+            </div>
+            <div className='flex flex-col space-y-4 contact-div m-auto'>
+              <h1 className='contact-text'>The <i><b>perfect</b></i> space for your event, workshop, meeting, or anything you need!</h1>
+              <div className='flex flex-row space-x-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
+                </svg>
+                <h1 className='contact-text'><b>240 878 0277</b></h1>
+              </div>
+              <div className='flex flex-row space-x-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+                </svg>
+                <h1 className='contact-text'>1423 Dual highway Hagerstown Md 21740</h1>
+              </div>
+              <input className='email-text-box' onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder="Email Address"/>  
+              
+              {isLoading? 
+              <div className="email-button flex flex-row justify-between cursor-pointer">
+                <span className="spinner-position spinner-position-alt">
+                  <div class="w-6 h-6 rounded-full animate-spin
+                    border border-solid border-black border-t-transparent"></div>
+                </span>
+              </div>
+              :
+              <button onClick={()=>submitEmail()} className='email-button'>Let's Talk!</button>         
+              }
+            </div>
+          </div>
         </div>
       </div>
     </>
