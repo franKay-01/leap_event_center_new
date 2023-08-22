c75855bf1ec90bc4f615b327713fae0d7f95355c
franKay-01 <fkay0450@gmail.com>
2023-08-22 15:16:21 +0000

    refact: changed logos on navbar

--- src/App.js
+++ src/App.js
@@ -79,24 +79,31 @@ function App() {
               </p>
           </div>
         </div>
-        <div class="contact-h contact-us-gradient mt-24">
+        <div class="contact-h contact-us-gradient mt-12 lg:mt-24 md:mt-24">
           <div className='container grid grid-cols-1'>
             <div className='flex flex-col'>
               <div className='contact-us-header'>Contact Us!</div>
             </div>
             <div id="targetSection" className='flex flex-col space-y-4 contact-div m-auto'>
               <h1 className='contact-text'>The <i><b>perfect</b></i> space for your event, workshop, meeting, or anything you need!</h1>
-              <div className='flex flex-row space-x-2'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8 hidden lg:flex md:flex">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
-                </svg>
-                <h1 className='contact-text'><b>240 878 0277</b></h1>
-              </div>
-              <div className='flex flex-row space-x-2'>
+              
+              <div className='flex flex-row space-x-0 lg:space-x-2 md:space-x-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8 hidden lg:flex md:flex">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
                 </svg>
                 <h1 className='contact-text'>1423 Dual highway Hagerstown Md 21740</h1>
+              </div>
+              <div className='flex flex-row space-x-0 lg:space-x-2 md:space-x-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8 hidden lg:flex md:flex">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
+                </svg>
+                <h1 className='contact-text'>info@leapeventscenter.com</h1>
+              </div>
+              <div className='flex flex-row space-x-0 lg:space-x-2 md:space-x-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8 hidden lg:flex md:flex">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
+                </svg>
+                <h1 className='contact-text'><b>240 878 0277</b></h1>
               </div>
               <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-8'>
                 <a target="_blank" rel="noreferrer" 
